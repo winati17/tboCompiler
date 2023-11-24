0b8899e5c14a283574d74ca01e7f44db94cdacd5
winati17 <winatimn@gmail.com>
2023-11-25 03:28:31 +0800

    ucapkan alhamdulillah

---
+++ compilersimple.py
@@ -0,0 +1,23 @@
+from lex import *
+from emit import *
+from parse import *
+import sys
+
+def main():
+    print("Selamat datang di Compiler Simple")
+
+    if len(sys.argv) != 2:
+        sys.exit("Error: Compiler needs source file as argument.")
+    with open(sys.argv[1], 'r') as inputFile:
+        source = inputFile.read()
+
+    # Initialize the lexer, emitter, and parser.
+    lexer = Lexer(source)
+    emitter = Emitter("out.c")
+    parser = Parser(lexer, emitter)
+
+    parser.program() # Start the parser.
+    emitter.writeFile() # Write the output to file.
+    print("Alhamdulillah compiling selesai.")
+
+main()
